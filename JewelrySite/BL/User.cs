3da2a019a74cf408fe38f0400b159d51afb77a6a
Dan <danchuk213@gmail.com>
2025-08-14 18:47:51 +0300

    added a User model

---
+++ JewelrySite/BL/User.cs
@@ -0,0 +1,15 @@
+﻿namespace JewelrySite.BL
+{
+	public class User
+	{
+		public int Id { get; set; } // Primary Key
+		public string Username { get; set; }
+		public string PasswordHash { get; set; }
+		public string Email { get; set; }
+		public string Role { get; set; } = "Customer";// e.g. "Customer", "Admin"
+		public DateTime CreatedAt { get; set; } = DateTime.Now;
+		public DateTime? LastLogin { get; set; }
+		public bool IsActive { get; set; } = true;
+	}
+}
+ 
