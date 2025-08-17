ab256f86b6a5b0284b8bd501f099c6b3ba11d6e0
dan khilkevich <danchuk213@gmail.com>
2025-08-17 17:50:08 +0300

    new DTO's

---
+++ JewelrySite/DTO/LoginResponseDto.cs
@@ -0,0 +1,8 @@
+﻿namespace JewelrySite.DTO
+{
+	public class LoginResponseDto
+	{
+		public required string JWTToken { get; set; }
+		public required string RefreshToken { get; set; }
+	}
+}
---
+++ JewelrySite/DTO/RefreshTokenDto.cs
@@ -0,0 +1,8 @@
+﻿namespace JewelrySite.DTO
+{
+	public class RefreshTokenDto
+	{
+		public int UserId { get; set; }
+		public required string RefreshToken { get; set; }
+	}
+}
