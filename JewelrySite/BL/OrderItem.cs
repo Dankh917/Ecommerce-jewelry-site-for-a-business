c761b7b2999b3af0f9a8430cce470fcd39aa82ff
dan khilkevich <danchuk213@gmail.com>
2025-08-19 19:38:57 +0300

    added models for checkout order further work needed

---
+++ JewelrySite/BL/OrderItem.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace JewelrySite.BL
+{
+	public class OrderItem
+	{
+		public int Id { get; set; }
+
+		[Required] public int OrderId { get; set; }
+		[JsonIgnore] public Order? Order { get; set; }
+
+		[Required] public int JewelryItemId { get; set; } // link back to product
+		[MaxLength(160)] public string? NameSnapshot { get; set; }    // title at purchase
+
+		[Required, Precision(18, 2)] public decimal UnitPrice { get; set; }
+		[Required, Range(1, int.MaxValue)] public int Quantity { get; set; }
+		[Required, Precision(18, 2)] public decimal LineTotal { get; set; } // UnitPrice * Quantity at checkout
+	}
+}
+
