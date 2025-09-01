c54a0286f6225c4cabb6540e5005214ead57e4a8
dan khilkevich <danchuk213@gmail.com>
2025-09-01 15:35:12 +0300

    no changes

--- JewelrySite/BL/JewelryItem.cs
+++ JewelrySite/BL/JewelryItem.cs
@@ -7,7 +7,7 @@ namespace JewelrySite.BL
 	{
 
 		public int Id { get; set; }
-		public required string Name { get; set; }
+		public required string Name { get; set; } 
 		public required string Description { get; set; }
 		public required string Category { get; set; }
 		public string? Collection { get; set; }
