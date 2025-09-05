cd0cbeb3661c542b609eba47e3e92dfc7bfa9932
dan khilkevich <danchuk213@gmail.com>
2025-09-05 14:41:52 +0300

    added mig

--- JewelrySite/Migrations/20250903131028_AddShippingPriceToJewelryItem.Designer.cs
+++ JewelrySite/Migrations/20250903131028_AddShippingPriceToJewelryItem.Designer.cs
@@ -4,6 +4,7 @@ using JewelrySite.DAL;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace JewelrySite.Migrations
 {
     [DbContext(typeof(JewerlyStoreDBContext))]
-    partial class JewerlyStoreDBContextModelSnapshot : ModelSnapshot
+    [Migration("20250903131028_AddShippingPriceToJewelryItem")]
+    partial class AddShippingPriceToJewelryItem
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -132,7 +135,7 @@ namespace JewelrySite.Migrations
                     b.Property<decimal?>("Price")
                         .HasColumnType("decimal(18,2)");
 
-                    b.Property<decimal>("ShippingPrice")
+                    b.Property<decimal?>("ShippingPrice")
                         .HasColumnType("decimal(18,2)");
 
                     b.Property<string>("SizeCM")
---
+++ JewelrySite/Migrations/20250903131028_AddShippingPriceToJewelryItem.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JewelrySite.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddShippingPriceToJewelryItem : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "ShippingPrice",
+                table: "JewelryItems",
+                type: "decimal(18,2)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ShippingPrice",
+                table: "JewelryItems");
+        }
+    }
+}
