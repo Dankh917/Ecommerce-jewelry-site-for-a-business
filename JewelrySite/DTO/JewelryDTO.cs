95bb98d83c4ae5c0bba53f4adf821e25b4a8f046
dan khilkevich <danchuk213@gmail.com>
2025-09-05 14:41:43 +0300

    added shippingPrice

--- JewelrySite/DTO/JewelryDTO.cs
+++ JewelrySite/DTO/JewelryDTO.cs
@@ -1,17 +1,17 @@
 ﻿namespace JewelrySite.DTO
 {
 	public record JewelryItemCatalogDto(
-	int Id,
-	string Name,
-	string? Description,
-	string Category,
-	string Collection,
-	decimal? Price,
-	int? StockQuantity,
-	bool? IsAvailable,
-	string MainImageUrl,
-	string? Color,
-	string? SizeCM
+		int Id,
+		string Name,
+		string? Description,
+		string Category,
+		string Collection,
+		decimal? Price,
+		int? StockQuantity,
+		bool? IsAvailable,
+		string MainImageUrl,
+		string? Color,
+		string? SizeCM,
+		decimal? ShippingPrice
 	);
-
 }
