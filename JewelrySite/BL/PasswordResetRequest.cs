605c4d92e62a731327d4f6a1ab1d388099f86d82
dan khilkevich <danchuk213@gmail.com>
2025-09-26 12:28:13 +0300

    added a PasswordResetRequest DTO for the "forgot my password" feature

---
+++ JewelrySite/BL/PasswordResetRequest.cs
@@ -0,0 +1,14 @@
+﻿using JewelrySite.BL;
+
+public class PasswordResetRequest
+{
+	public int Id { get; set; }
+	public int UserId { get; set; }
+
+	// Store only the hash of the random token
+	public string TokenHash { get; set; } = default!;
+	public DateTime ExpiresAtUtc { get; set; }
+	public DateTime CreatedAtUtc { get; set; }
+	public DateTime? UsedAtUtc { get; set; }
+	
+}
