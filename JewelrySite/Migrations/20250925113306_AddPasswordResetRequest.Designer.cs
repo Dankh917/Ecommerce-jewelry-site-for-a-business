fcd5075b1899bd8f4fc03717c928ced3c0ad8d54
dan khilkevich <danchuk213@gmail.com>
2025-09-26 12:44:03 +0300

    migrations are jsut migrations ... added new PasswordResetRequest table

--- JewelrySite/Migrations/20250925113306_AddPasswordResetRequest.Designer.cs
+++ JewelrySite/Migrations/20250925113306_AddPasswordResetRequest.Designer.cs
@@ -4,6 +4,7 @@ using JewelrySite.DAL;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace JewelrySite.Migrations
 {
     [DbContext(typeof(JewerlyStoreDBContext))]
-    partial class JewerlyStoreDBContextModelSnapshot : ModelSnapshot
+    [Migration("20250925113306_AddPasswordResetRequest")]
+    partial class AddPasswordResetRequest
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ JewelrySite/Migrations/20250925113306_AddPasswordResetRequest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JewelrySite.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPasswordResetRequest : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PasswordResetRequests",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    TokenHash = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ExpiresAtUtc = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatedAtUtc = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UsedAtUtc = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PasswordResetRequests", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PasswordResetRequests");
+        }
+    }
+}
